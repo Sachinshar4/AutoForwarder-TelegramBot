a893999a9dac346685eaf9c1962a277064953dec
Sachinshar4 <130998113+Sachinshar4@users.noreply.github.com>
2025-11-25 18:03:55 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+auto_forwarder.sample_config import Config
+from auto_forwarder.sample_config import Config
+
+
+class Development(Config):
+    API_KEY = "8220844170:AAEmGmcf_-aDHlM-ibTjwpPcXstZGajPRoM"  # My bot API key
+    OWNER_ID = 6436766847  # My user id
+
+    # Make sure to include the '-' sign in group and channel ids.
+    FROM_CHATS = [-1001234567890]  # List of chat id's to forward messages from.
+    TO_CHATS = [-1001234567890 , 1234567890]  # List of chat id's to forward messages to.
+    
+    WORKERS = 4
