5c7978bd81ba89b2885ae26af7cdf2b8e1d455aa
Sachinshar4 <130998113+Sachinshar4@users.noreply.github.com>
2025-11-25 18:22:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Development(Config):
     OWNER_ID = 6436766847  # My user id
 
     # Make sure to include the '-' sign in group and channel ids.
-    FROM_CHATS = [-1001234567890]  # List of chat id's to forward messages from.
-    TO_CHATS = [-1001234567890 , 1234567890]  # List of chat id's to forward messages to.
+    FROM_CHATS = [-https://t.me/latestupsc1/3]  # List of chat id's to forward messages from.
+    TO_CHATS = [-@Mishra0078, 1234567890]  # List of chat id's to forward messages to.
     
     WORKERS = 4
